ee1646831e3f3754786d6086fc6c827151463f29
Voss <voss@visnovalabs.io>
2022-11-24 15:00:10 -0600

    add fee recipient addr
    fixes #13

--- beacon-chain/entrypoint.sh
+++ beacon-chain/entrypoint.sh
@@ -21,6 +21,7 @@ exec /opt/teku/bin/teku \
     --metrics-port 8008 \
     --metrics-host-allowlist "*" \
     --log-destination=CONSOLE \
+    --validators-proposer-default-fee-recipient="${FEE_RECIPIENT_ADDRESS}" \
     --p2p-discovery-bootnodes=enr:-KG4QBHSWcjL-sdK83SPvRTmL1b175sS6LoXykFZ_hjzpJAFO6IUQ55vVX7uSU2_ZP6uH03Pxey_YC-mwro_VmNheg4DhGV0aDKQSOoWfAEAAG9aAAAAAAAAAIJpZIJ2NIJpcISqu5rviXNlY3AyNTZrMaED_dNGAGGvZHb85Uiho1aPhd11cwE4zmKdwbIy40qKYTuDdGNwgiMog3VkcIIjKA \
     --p2p-discovery-bootnodes=enr:-L64QOijsdi9aVIawMb5h5PWueaPM9Ai6P17GNPFlHzz7MGJQ8tFMdYrEx8WQitNKLG924g2Q9cCdzg54M0UtKa3QIKCMxaHYXR0bmV0c4j__________4RldGgykDE2cEMCAABv__________-CaWSCdjSCaXCEi5AaWYlzZWNwMjU2azGhA8CjTkD4m1s8FbKCN18LgqlYcE65jrT148vFtwd9U62SiHN5bmNuZXRzD4N0Y3CCIyiDdWRwgiMo \
     --p2p-discovery-bootnodes=enr:-L64QKYKGQj5ybkfBxyFU5IEVzP7oJkGHJlie4W8BCGAYEi4P0mmMksaasiYF789mVW_AxYVNVFUjg9CyzmdvpyWQ1KCMlmHYXR0bmV0c4j__________4RldGgykDE2cEMCAABv__________-CaWSCdjSCaXCEi5CtNolzZWNwMjU2azGhAuA7BAwIijy1z81AO9nz_MOukA1ER68rGA67PYQ5pF1qiHN5bmNuZXRzD4N0Y3CCIyiDdWRwgiMo \
